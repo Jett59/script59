973c84906eec2a12ba46f47dce84b9b57d060b2a
Jett Thompson <jett.thompson.59@gmail.com>
2021-06-27 08:13:44 +1000

    added a division step which divides the list of tokens into a list of statements (lists of tokens) to make the job of the parser a lot easier

--- src/main/java/app/cleancode/script59/Start.java
+++ src/main/java/app/cleancode/script59/Start.java
@@ -1,6 +1,9 @@
 package app.cleancode.script59;
 
+import java.util.List;
+import app.cleancode.script59.divider.Divider;
 import app.cleancode.script59.lex.Lexer;
+import app.cleancode.script59.lex.Token;
 
 public class Start {
     public static void main(String[] args) {
@@ -17,7 +20,12 @@ public class Start {
                         .lex("public int myInt = 4 * 5; System.out.println (\"Hello there!\\t\");");
                 long time = System.nanoTime() - start;
                 System.out.printf("Lex: Time taken: %.3fS\n", time / 1000000000d);
-                System.out.println(tokens);
+                Divider divider = new Divider();
+                start = System.nanoTime();
+                List<List<Token>> dividedTokens = divider.divide(tokens);
+                time = System.nanoTime() - start;
+                System.out.printf("Divide: Time taken: %.3fS\n", time / 1000000000d);
+                System.out.println(dividedTokens);
                 break;
             }
         }
--- src/main/java/app/cleancode/script59/parse/AbstractSyntaxTree.java
+++
@@ -1,5 +0,0 @@
-package app.cleancode.script59.parse;
-
-public class AbstractSyntaxTree {
-
-}
