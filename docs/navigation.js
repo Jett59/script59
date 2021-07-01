a714db95045834ef878c2e5e300b5dc059150091
Jett Thompson <jett.thompson.59@gmail.com>
2021-07-01 13:58:03 +1000

    added a basic home page and download site

---
+++ docs/navigation.js
@@ -0,0 +1,16 @@
+function loadNavigation () {
+document.getElementById('navigation').innerHTML = '\
+<nav>\
+<table>\
+<tr>\
+<td> \
+<a href=\"/index.html\">Home</a> \
+    </td> \
+    <td>\
+        <a href=\"/download.html\">Download</a>\
+    </td>\
+</tr>\
+</table>\
+</nav>\
+';
+}
